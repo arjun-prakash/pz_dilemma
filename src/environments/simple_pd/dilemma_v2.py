89e06f19fcf9164a0e237bcb92aa6e3682ab78c7
Arjun <apra3637@uni.sydney.edu.au>
2021-06-20 17:23:39 +1000

    partner selection

--- src/environments/simple_pd/dilemma_v2.py
+++ src/environments/simple_pd/dilemma_v2.py
@@ -34,9 +34,10 @@ class raw_env(AECEnv):
         self.possible_agents = self.agents[:]
         self.agent_name_mapping = dict(zip(self.agents, list(range(self.num_agents))))
 
-        self.action_spaces = {agent: spaces.Dict({'dilemma':spaces.Discrete(2),'selector':spaces.Discrete(n_agents)}) for agent in self.agents}
-        self.observation_spaces = {agent: spaces.Dict({'dilemma':spaces.Discrete(3),'selector':spaces.Discrete(n_agents)}) for agent in self.agents}
+        self.action_spaces = {agent: spaces.MultiDiscrete([2,n_agents]) for agent in self.agents}
+        self.observation_spaces = {agent: spaces.Box(low=-1, high=1, shape=(1,n_agents), dtype=np.int8) for agent in self.agents}
         print('Delimma v2')
+        print(self.observation_spaces)
         self.reinit()
 
     def reinit(self):
@@ -47,21 +48,20 @@ class raw_env(AECEnv):
         self._cumulative_rewards = {agent: 0 for agent in self.agents}
         self.dones = {agent: False for agent in self.agents}
         self.infos = {agent: {} for agent in self.agents}
-        self.state = {agent: {'dilemma':self.game.DEFECT} for agent in self.agents}
-        self.observations = {agent: {'dilemma':self.game.DEFECT} for agent in self.agents}
+        self.state = {agent: [0,0] for agent in self.agents}
+        self.observations = {agent: np.zeros(len(self.agents)) for agent in self.agents}
         self.num_moves = 0
 
     def render(self, mode="human"):
         try:
             #print('dones', self.dones)
             for i in self.agents:
-                string_moves = ("Current moves: {} : {}".format(i, self.game.moves[self.state[self.agents[self.agent_name_mapping[i]]]['dilemma']]))
-                string_partners = ("Current partners of: {} , {}".format(i, self.agents[self.state[self.agents[self.agent_name_mapping[i]]]['selector']]))
-
+                #string_partners = ("Current partners of: {} , {}".format(i, self.agents[self.state[self.agents[self.agent_name_mapping[i]]][1]]))
+                string_moves = ("Current moves: {} : {}, {}, {}".format(i, self.game.moves[self.state[self.agents[self.agent_name_mapping[i]]][0]], self.agents[self.state[self.agents[self.agent_name_mapping[i]]][1]],self.game.moves[self.state[self.agents[self.state[self.agents[self.agent_name_mapping[i]]][1]]][0]]))
                 print(string_moves)
-                print(string_partners)
+                #print(string_partners)
 
-            print(self.rewards)
+            print('rewards', self.rewards)
             print("")
             return self.dones
         except:
@@ -71,6 +71,7 @@ class raw_env(AECEnv):
         # observation of one agent is the previous state of the other
         #print('agent ', agent)
         #print('observation' , np.array(self.observations[agent]))
+        #print('observations', self.observations)
         return np.array(self.observations[agent])
 
 
@@ -88,8 +89,8 @@ class raw_env(AECEnv):
 
         agent = self.agent_selection
         self.state[self.agent_selection] = action
-        dilemma_action = self.state[self.agent_selection]['dilemma']
-        selector_action = self.state[self.agent_selection]['selector']
+        dilemma_action = self.state[self.agent_selection][0]
+        selector_action = self.state[self.agent_selection][1]
         # collect reward if it is the last agent to act
 
         agent_index = self._agent_selector._current_agent
@@ -108,16 +109,28 @@ class raw_env(AECEnv):
             #need to do both?
 
             # observe the current state
+
             for i in self.agents:
-                self.observations[i] = self.state[self.agents[1 - self.agent_name_mapping[i]]]
-                print('obs',self.observations[i])
+                if self.state[self.agents[agent_index]][0] == 0:
+                    self.observations[i][agent_index]+=1
+                if self.state[self.agents[agent_index]][0] == 1:
+                    self.observations[i][agent_index]-=1
 
+                if self.state[self.agents[selector_action]][0] == 0:
+                        self.observations[i][selector_action]+= 1
+                if self.state[self.agents[selector_action]][0] == 1:
+                        self.observations[i][selector_action]-= 1
 
-            self.rewards[self.agents[agent_index]], self.rewards[self.agents[selector_action]] = self.game.get_payoff()[(self.state[self.agents[agent_index]]['dilemma'],self.state[self.agents[selector_action]]['dilemma'])]
+                #self.observations[i] = self.state[self.agents[1 - self.agent_name_mapping[i]]]
+                #self.observations[i]['dilemma'] = 1
+            #    self.observations[i]['dilemma'] = [0,0,0,1]
+
+            self.rewards[self.agents[agent_index]], self.rewards[self.agents[selector_action]] = self.game.get_payoff()[(self.state[self.agents[agent_index]][0],self.state[self.agents[selector_action]][0])]
+
             #self.state[self.agents[1 - self.agent_name_mapping[agent]]] = self.game.NONE
 
 
-            self._clear_rewards()
+            #self._clear_rewards()
 
         self._cumulative_rewards[self.agent_selection] = 0
         self.agent_selection = self._agent_selector.next()
