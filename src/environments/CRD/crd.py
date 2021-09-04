5db20f0f0226eee994159862692dbcff5da5f391
Arjun <apra3637@uni.sydney.edu.au>
2021-09-04 17:59:57 +1000

    working crd

--- src/environments/CRD/crd.py
+++ src/environments/CRD/crd.py
@@ -30,9 +30,13 @@ class raw_env(AECEnv):
 
         self.pool = 0
         self.c = 0.1
-        self.t = 200
-        self.r = 0.5
+        self.r = 0.9
         self.p = 0.7
+
+        self.M = n_agents/2
+
+        self.t = self.M * self.c * endowment
+
 
         self.agents = ["player_" + str(r) for r in range(n_agents)]
         self.possible_agents = self.agents[:]
@@ -40,7 +44,7 @@ class raw_env(AECEnv):
 
         self.action_spaces = {agent: spaces.Discrete(2) for agent in self.agents}
         self.observation_spaces = {agent: spaces.Discrete(3) for agent in self.agents}
-        print('Delimma!')
+        print('crd!')
         print(self.observation_spaces)
         self.state = {agent: 2 for agent in self.agents}
         self.endowment = {agent: endowment for agent in self.agents}
@@ -48,6 +52,8 @@ class raw_env(AECEnv):
         self.reinit()
 
     def reinit(self):
+        print('reinit')
+        self.pool=0
         self.agents = self.possible_agents[:]
         self._agent_selector = agent_selector(self.agents)
         self.agent_selection = self._agent_selector.next()
@@ -56,14 +62,18 @@ class raw_env(AECEnv):
         self.dones = {agent: False for agent in self.agents}
         self.infos = {agent: {} for agent in self.agents}
         self.observations = {agent: 2 for agent in self.agents} #placeholder
+        self.state = {agent: 2 for agent in self.agents} #placeholder
+        self.endowment = {agent: 100 for agent in self.agents}
+
+
         self.num_moves = 0
 
     def render(self, mode="human"):
         try:
             #print('dones', self.dones)
             for i in self.agents:
-                str_moves = ("Current moves of: {} , {} with {}".format(i, MOVES[self.state[i]], self.endowment[i]))
-                str_pool = ("Pool is: {} , Threshold is: {}".format(self.pool, self.t))
+                str_moves = ("Agent {}, state: {}, obs: {} with {}".format(i, MOVES[self.state[i]], self.observations[i] , self.endowment[i]))
+                str_pool = ("Pool is: {} , Threshold is: {}, outcome is: {}".format(self.pool, self.t, self.is_disaster))
                 print(str_moves)
             print('rewards', self.rewards)
             print(str_pool)
@@ -93,54 +103,50 @@ class raw_env(AECEnv):
 
         agent = self.agent_selection
 
-        if action == 0: #DEFECT
-            self.state[agent] = 0
-        elif action == 1: #COOPERATE
-            self.state[agent] = 1
+        self.state[agent] = action
+
+
+        if action == 1: #COOPERATE
             contribution = self.endowment[agent] * self.c
             self.endowment[agent] = self.endowment[agent] - contribution
             self.pool += contribution
 
-        self.observations[agent] = action
-
-
-
 
         # collect reward if it is the last agent to act
         if self._agent_selector.is_last():
+            self.dones = {agent: 1 for agent in self.agents}
+
 
             if self.pool > self.t:
                 self.omega = 1
-                is_disaster = 0
-                self.dones = {agent: 1 for agent in self.agents}
+                self.is_disaster = 0
 
             else:
                 self.omega = 0
-                is_disaster = np.random.choice(2,  p=[self.r, 1-self.r])
+                self.is_disaster = np.random.choice(2,  p=[1 - self.r, self.r])
 
             for i in self.agents:
-                if not is_disaster:
-                    if self.observations[i] == 0:
+                self.observations[i] = self.is_disaster
+                if not self.is_disaster:
+                    if self.state[i] == 0:
                         self.rewards[i] = 0
-                    elif self.observations[i] == 1:
+                    elif self.state[i] == 1:
                         self.rewards[i] = np.log(1 - self.c)
 
-                if is_disaster:
-                    if self.observations[i] == 0:
+                if self.is_disaster:
+                    if self.state[i] == 0:
                         self.rewards[i] = np.log(1 - self.p)
-                    elif self.observations[i] == 1:
+                    elif self.state[i] == 1:
                         self.rewards[i] = np.log(1 - self.c - self.p + (self.c*self.p))
 
 
 
-            self.num_moves += 1
-            self.dones = {agent: is_disaster for agent in self.agents}
-
 
         else:
             for i in self.agents:
+                self.infos[i]['pool'] = self.pool
                 if self.agent_name_mapping[i] > self.agent_name_mapping[agent]:
-                    self.state[i] = 2
+                    self.observations[i] = 2
                     #self.observations[self.agent_name_mapping[i]] = 2
 
 
